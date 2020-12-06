57b314060e93ec347d2d86b6c7a8a1ec75473848
Santosh S Malagi <santoshsmalagi@gmail.com>
2020-12-05 21:17:26 -0500

    Create PrintEnvironment.c

---
+++ 13.System-Calls-and-Kernel/PrintEnvironment.c
@@ -0,0 +1,28 @@
+```C
+/*****************************************************************************
+/* C Program to print the environment variables and values stored in them,   *                 
+/* for the current terminal session.                                         *
+/****************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[], char *envp[])
+{
+
+  printf("\n------------------------------------------------------------------------\n");
+  printf("The environment:\n");
+  
+  int i =0;
+  while(envp[i] != NULL)
+  {
+    printf("%s\n", envp[i]);
+    i++;
+  }
+
+  printf("\n------------------------------------------------------------------------\n");
+  return 0;
+  
+}
+
+```
